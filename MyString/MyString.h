eed114a33ecabbbe877ae442a64b74fc0923a547
Luke Rodman <luke.rodman@eagles.oc.edu>
2019-02-27 13:06:27 -0600

    update

--- MyString/MyString.h
+++ MyString/MyString.h
@@ -1,3 +1,10 @@
 #pragma once
 // MyString class declaration goes here
-
+class MyString
+{
+private:
+	char* ptr;
+public:
+	MyString();
+	MyString(char*)
+};
--- MyString/testMyString.cpp
+++ MyString/testMyString.cpp
@@ -1,1 +1,13 @@
-// test driver code goes here
+#include "MyString.h"
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+
+
+	cout << endl; system("pause"); return 0;
+}
